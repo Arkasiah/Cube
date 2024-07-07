fcf41671097738aa8644d26feda6a8af9fa04a1e
GUILLAUME <66298241+Arkasiah@users.noreply.github.com>
2024-07-07 22:24:55 +0200

    fixing build for auction

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -53,7 +53,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";
     });
-builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
+// builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
 builder.Services.AddGrpc();
 
     
