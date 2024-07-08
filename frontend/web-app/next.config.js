75b9e6a4dd83d7c5593244242c58bff7e0f29770
GUILLAUME <66298241+Arkasiah@users.noreply.github.com>
2024-07-08 11:23:05 +0200

    another attempt

--- frontend/web-app/next.config.js
+++ frontend/web-app/next.config.js
@@ -1,5 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Acess-Control-Allow-Credentials',
+                        value: 'true'
+                    },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*'
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,DELETE,PATCH,POST,PUT'
+                    },
+                    {
+                        key: 'Aceess-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+                    }
+                ]
+            }
+        ];
+    },
     experimental: {
         serverActions: true
     },
